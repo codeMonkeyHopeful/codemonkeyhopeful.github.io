0682244e0858eeb7cc61495b189630f154799771
Ryan Jasinski <jasinski.17@gmail.com>
2025-03-05 23:02:58 -0800

    add text input fields

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,29 @@ function App() {
         <div className="col">
           <p className="lead">{"HTML Gives Your Code Structure"}</p>
           <strong>{"HTML"}</strong>
+          <textarea
+            className="form-control"
+            placeholder="HTML Goes Here"
+            id="floatingTextarea"
+          ></textarea>
         </div>
         <div className="col">
           <p className="lead">{"CSS Gives Your Code Style"}</p>
           <strong>{"CSS"}</strong>
+          <textarea
+            className="form-control"
+            placeholder="CSS Goes Here"
+            id="floatingTextarea"
+          ></textarea>
         </div>
         <div className="col">
           <p className="lead">{"Javascript Gives Your Code Action"}</p>
           <strong>{"Javascript"}</strong>
+          <textarea
+            className="form-control"
+            placeholder="Yup You Got It, Javascript Goes Here"
+            id="floatingTextarea"
+          ></textarea>
         </div>
       </div>
     </div>
