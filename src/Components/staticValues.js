efae46209fa41d224937c406ef2d2d572db467ea
Ryan Jasinski <jasinski.17@gmail.com>
2025-03-06 22:17:32 -0800

    add hardcoded lookup

---
+++ src/Components/staticValues.js
@@ -0,0 +1,7 @@
+import js from "@eslint/js";
+
+export const columnHeaderMapping = {
+  css: { name: "CSS", descrtipion: "CSS Gives Your Code Style" },
+  html: { name: "HTML", descrtipion: "HTML Gives Your Code Structure" },
+  js: { name: "JS", descrtipion: "JS Gives Your Code Functionality" },
+};
