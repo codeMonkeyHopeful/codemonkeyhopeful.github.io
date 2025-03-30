361a2b0b3d8433085cfb272f057acc340fb4b60a
Ryan Jasinski <jasinski.17@gmail.com>
2025-03-30 04:55:12 -0700

    commit automation

--- scripts/createCommitMessge.js
+++ scripts/createCommitMessge.js
@@ -7,4 +7,9 @@ const dateTime = moment().format("YYYY-MM-DD HH:mm:ss");
 // Get the username of the person who pushed the code
 const username = execSync("git config user.name", { encoding: "utf-8" }).trim();
 
-console.log(`Production launch: ${dateTime} by user: ${username}`);
+console.log(`"Production launch: ${dateTime} by user: ${username}"`);
+
+execSync(`git add .`);
+// Commit the changes with the generated message
+execSync(`git commit -m "Production launch: ${dateTime} by user: ${username}"`);
+execSync(`git push origin gh-pages`);
