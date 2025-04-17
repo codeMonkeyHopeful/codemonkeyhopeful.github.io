6f537b77e68ac306534e134f4c6fe8fd44ab2b34
Ryan Jasinski <jasinski.17@gmail.com>
2025-04-16 22:03:10 -0700

    main script error checking

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,2 +1,18 @@
 // This file is our main file for publishing the project. It will run the scripts in the correct order and handle errors.
 import { exec } from "child_process";
+import chalk from "chalk";
+import { restoreFromMain } from "./index.js";
+
+console.log("Running scripts. Time is " + new Date().toLocaleTimeString());
+
+try {
+  const restoreFromMainStatus = await restoreFromMain();
+} catch (error) {
+  console.error(
+    `Something went wrong while ${chalk.purple(
+      "restoring"
+    )} from ${chalk.purple(
+      "main"
+    )} branch. Time is ${new Date().toLocaleTimeString()} - ${error.message}`
+  );
+}
