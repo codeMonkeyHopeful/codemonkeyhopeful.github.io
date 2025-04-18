c8b47916832f6daec383a3e2ec856825f4c716fd
Ryan Jasinski <jasinski.17@gmail.com>
2025-04-17 21:06:21 -0700

    updating values on chang

--- src/Components/Containers/CodeWindow.tsx
+++ src/Components/Containers/CodeWindow.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+interface CodeState {
+  code: string;
+  language: string;
+}
 export const CodeWindow: React.FC<{ codeLanguage: string }> = ({
   codeLanguage,
 }) => {
+  const [code, setCode] = useState<CodeState>({
+    code: "",
+    language: codeLanguage,
+  });
+
+  useEffect(() => {
+    // TODO pass the new value into the IFrame Window
+  }, [code]);
+
+  const updateCode = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    setCode((prevState) => ({
+      ...prevState,
+      code: (e.target as HTMLTextAreaElement).value,
+    }));
+  };
+
   return (
     <div className="col" key={codeLanguage}>
       <p className="lead">{`${
@@ -10,11 +30,12 @@ export const CodeWindow: React.FC<{ codeLanguage: string }> = ({
       } Gives Your Code ${
         codeLanguage === "javascript" ? "Action" : "Style"
       }`}</p>
-      <strong>{codeLanguage.toUpperCase()}</strong>
+
       <textarea
+        onKeyUp={updateCode}
         className="form-control"
         placeholder={`${codeLanguage.toUpperCase()} Goes Here`}
-        id="floatingTextarea"
+        id={`${codeLanguage.toLocaleLowerCase()}-input`}
       ></textarea>
     </div>
   );
